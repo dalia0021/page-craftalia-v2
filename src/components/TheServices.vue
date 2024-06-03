8ffe1a4ce5dafd577879478895259ee3f4470616
unknown <dalia@saitenlinea.com>
2024-06-03 16:08:06 -0700

    seccion servicios

--- src/components/TheServices.vue
+++ src/components/TheServices.vue
@@ -2,20 +2,23 @@
     <div class="bg-white py-20">
       <div class="mx-auto max-w-7xl px-6 lg:px-8">
         <div class="mx-auto max-w-2xl lg:text-center">
-          <h2 class="text-base leading-7 text-amber-600 text-matcha">¡Bienvenido a craftalia!</h2>
-          <p class="mt-2 text-3xl font-bold tracking-tight text-amber-900 sm:text-4xl">Conozca nuestros servicios</p>
-          <p class="mt-6 text-lg leading-8 text-gray-600">Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum pulvinar et feugiat blandit at. In mi viverra elit nunc.</p>
+          <h2 class="text-center text-base leading-7 text-amber-600 text-matcha">¡Bienvenido a craftalia!</h2>
+          <p class="text-center mt-2 text-3xl font-bold tracking-tight text-amber-900 sm:text-4xl">¿Por Qué Elegir Artículos Hechos a Mano?</p>
+          <!-- <p class="mt-4 text-lg leading-8 text-gray-600">Al elegir productos hechos a mano .</p> -->
         </div>
-        <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
-          <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
-            <div v-for="feature in features" :key="feature.name" class="relative pl-16">
-              <dt class="text-base font-semibold leading-7 text-gray-900">
-                <div class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-amber-600">
-                  <component :is="feature.icon" class="h-6 w-6 text-white" aria-hidden="true" />
-                </div>
-                {{ feature.name }}
-              </dt>
-              <dd class="mt-2 text-base leading-7 text-gray-600">{{ feature.description }}</dd>
+        <div class="mx-auto mt-8">
+          <dl class="flex flex-col lg:flex-row lg:space-x-10"> 
+            <div  v-for="(item, idx) in servicios" :key="idx"  
+                class="text-center p-6 space-y-3">
+                <span class="inline-block text-amber-500 dark:text-amber-400">
+                   <component :is="item.icon" class="h-10 w-10" aria-hidden="true" />
+                </span>
+
+                <h1 class="text-xl font-semibold text-gray-700 capitalize dark:text-white">{{item.name}}</h1>
+
+                <p class="text-gray-500 dark:text-gray-300">
+                    {{item.desc}}
+                </p>
             </div>
           </dl>
         </div>
@@ -24,32 +27,24 @@
   </template>
   
   <script setup>
-  import { ArrowPathIcon, CloudArrowUpIcon, FingerPrintIcon, LockClosedIcon } from '@heroicons/vue/24/outline'
+  import { ref } from "vue";
+  import { GiftIcon, HeartIcon, SparklesIcon } from '@heroicons/vue/24/outline'
+
+  const servicios = ref([
+    {
+        name:"Calidad y Cuidado",
+        desc:"Elaborados cuidadosamente y con atencion a cada detalle. Para que asi puedas obtener un producto con calidad.",
+        icon: SparklesIcon
+    },{
+        name:"Regalos Significativos",
+        desc:"Cada pieza es creada con amor y cariño, mostrandoles a tus seres queridos cuánto te importan con un detalle especial.",
+        icon:GiftIcon
+    }, {
+        name:"Apoyo a la artesania",
+        desc:"Al elegir productos hechos a mano, estás contribuyendo a crear y mantener a los pequeños negocios",
+        icon:HeartIcon
+    }
+  ])
   
-  const features = [
-    {
-      name: 'Push to deploy',
-      description:
-        'Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.',
-      icon: CloudArrowUpIcon,
-    },
-    {
-      name: 'SSL certificates',
-      description:
-        'Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.',
-      icon: LockClosedIcon,
-    },
-    {
-      name: 'Simple queues',
-      description:
-        'Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.',
-      icon: ArrowPathIcon,
-    },
-    {
-      name: 'Advanced security',
-      description:
-        'Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.',
-      icon: FingerPrintIcon,
-    },
-  ]
+ 
   </script>
